d4819a3ed70540ac537fd43b8d65e91f4af38528
Peter Kinsella <pallesnik@gmail.com>
2020-03-02 15:30:16 +0000

    Trainer implemented but not working

--- deep_learning/deep_learning.py
+++ deep_learning/deep_learning.py
@@ -5,8 +5,14 @@ import cv2
 from tqdm import tqdm
 import pickle
 import random
+import tensorflow as tf
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
 
-DATADIR = "/home/dataset/PetImages"
+DATADIR = "PetImages/"
 
 CATEGORIES = ["Dog", "Cat"]
 
@@ -45,7 +51,7 @@ for features, label in training_data:
     X.append(features)
     y.append(label)
 
-X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+X = np.array(X).reshape(-1, IMG_SIZE)
 
 pickle_out = open("X.pickle", "wb")
 pickle.dump(X, pickle_out)
@@ -60,3 +66,28 @@ X = pickle.load(pickle_in)
 
 pickle_in = open("y.pickle", "rb")
 y = pickle.load(pickle_in)
+
+X = X/255.0
+
+model = Sequential()
+
+model.add(Conv2D(256, (3, 3), input_shape=X.shape[1:]))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(256, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
+
+model.add(Dense(64))
+
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+model.fit(X, y, batch_size=32, epochs=4, validation_split=0.3)
