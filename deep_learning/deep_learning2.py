5eeb263a4628415943d034d73294f7a519830f3e
Peter Kinsella <pallesnik@gmail.com>
2020-03-21 14:56:35 +0000

    minor changes for testing

--- deep_learning/deep_learning2.py
+++ deep_learning/deep_learning2.py
@@ -30,13 +30,19 @@ def save():
 def create_model():
     classifier = Sequential()  # Initialising the CNN
     # Step 1 - Convolution
-    classifier.add(Conv2D(32, (3, 3), input_shape=(64, 64, 3), activation='relu'))
+    classifier.add(Conv2D(2, (3, 3), input_shape=(64, 64, 3), activation='relu'))
 
     # Step 2 - Pooling
     classifier.add(MaxPooling2D(pool_size=(2, 2)))
 
     # Adding a second convolutional layer
-    classifier.add(Conv2D(32, (3, 3), activation='relu'))
+    classifier.add(Conv2D(2, (3, 3), activation='relu'))
+    classifier.add(MaxPooling2D(pool_size=(2, 2)))
+
+    classifier.add(Conv2D(2, (3, 3), activation='relu'))
+    classifier.add(MaxPooling2D(pool_size=(2, 2)))
+
+    classifier.add(Conv2D(2, (3, 3), activation='relu'))
     classifier.add(MaxPooling2D(pool_size=(2, 2)))
 
     # Step 3 - Flattening
@@ -56,14 +62,14 @@ def train_data(classifier):
 
     test_datagen = ImageDataGenerator(rescale=1. / 255)
 
-    training_set = train_datagen.flow_from_directory('dataset/training_set',
-                                                     target_size=(64, 64), batch_size=32, class_mode='binary')
+    training_set = train_datagen.flow_from_directory('dataset/rps_train',
+                                                     target_size=(64, 64), batch_size=2, class_mode='binary')
 
-    test_set = test_datagen.flow_from_directory('dataset/test_set',
-                                                target_size=(64, 64), batch_size=32, class_mode='binary')
+    test_set = test_datagen.flow_from_directory('dataset/rps_test',
+                                                target_size=(64, 64), batch_size=2, class_mode='binary')
 
-    classifier.fit_generator(training_set, steps_per_epoch=8000,
-                             epochs=5, validation_data=test_set, validation_steps=2000)
+    classifier.fit_generator(training_set, steps_per_epoch=1000,
+                             epochs=5, validation_data=test_set, validation_steps=1000)
 
     save()
 
