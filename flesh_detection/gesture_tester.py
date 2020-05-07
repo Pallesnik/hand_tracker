597ec611b6142abef56efd30aa76268c44fe5dc2
Peter Kinsella <pallesnik@gmail.com>
2020-05-07 12:03:50 +0100

    Added testing file and updated flesh_detector.py

--- flesh_detection/gesture_tester.py
+++ flesh_detection/gesture_tester.py
@@ -1,13 +1,14 @@
 import argparse
 import datetime
 import time
+import csv
 import cv2
 import numpy as np
 import math
+import xlrd
 import matplotlib.pyplot as plt
 
 vc = cv2.VideoCapture("lubin.mp4")  # video capture [1]
-
 width = np.int(vc.get(cv2.CAP_PROP_FRAME_WIDTH))  # float
 height = np.int(vc.get(cv2.CAP_PROP_FRAME_HEIGHT))  # float
 Nframes = np.int(vc.get(cv2.CAP_PROP_FRAME_COUNT))
@@ -21,6 +22,7 @@ out = cv2.VideoWriter("output.mp4", fourcc, FPS, dim, True)
 points_to_track = [0] *30
 K = 20
 f = 0
+prevFrame = 0
 lk_params = dict(winSize=(15,15),maxLevel=2,criteria=(cv2.TERM_CRITERIA_EPS|cv2.TERM_CRITERIA_COUNT,10,0.01))
 criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_MAX_ITER, 10, 0.1)
 num_frames = 0
@@ -31,6 +33,12 @@ scissors = 0
 def_rock = 0
 def_paper = 0
 def_scissors = 0
+label_test = 3
+correct_frames = 0
+
+loc = ("test_labels.xlsx")
+wb = xlrd.open_workbook(loc)
+sheet = wb.sheet_by_index(0)
 
 while check:
     if frame is not None:
@@ -94,8 +102,6 @@ while check:
                         if angle <= 90:
                             num_def += 1
                             cv2.circle(resized, far, 5, [255, 0, 0], -1)
-                            #points_to_track[k] = far
-                            #k = k + 1
 
                         cv2.line(num_def, start, end, [0, 255, 0], 2)
                         cv2.drawContours(resized, [contours[ci]], 0, (0, 255, 0), 2)
@@ -109,101 +115,59 @@ while check:
                     paper = paper + 1
                     rock = 0
                     scissors = 0
-                    certain_frames = certain_frames + 1
-                    if paper >= 5:
+                    label_test = 1
+                    if paper == 10:
                         cv2.putText(resized, 'Paper', (0, 100), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 2)
+                        certain_frames = certain_frames + 10
+                    elif paper > 10:
+                        cv2.putText(resized, 'Paper', (0, 100), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 2)
+                        certain_frames = certain_frames + 1
                     else:
                         cv2.putText(resized, 'Paper', (0, 100), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
                 elif num_def == 2:
                     paper = 0
                     rock = 0
                     scissors = scissors + 1
-                    certain_frames = certain_frames + 1
-                    if scissors >= 5:
+                    label_test = 2
+                    if scissors == 10:
                         cv2.putText(resized, 'Scissors', (0, 100), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 2)
+                        certain_frames = certain_frames + 10
+                    elif scissors > 10:
+                        cv2.putText(resized, 'Scissors', (0, 100), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 2)
+                        certain_frames = certain_frames + 1
                     else:
                         cv2.putText(resized, 'Scissors', (0, 100), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
                 elif num_def == 0:
                     paper = 0
                     rock = rock + 1
                     scissors = 0
-                    certain_frames = certain_frames + 1
-                    if rock >= 5:
+                    label_test = 0
+                    if rock == 10:
                         cv2.putText(resized, 'Rock', (0, 100), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 2)
+                        certain_frames = certain_frames + 10
+                    elif rock > 10:
+                        cv2.putText(resized, 'Rock', (0, 100), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 2)
+                        certain_frames = certain_frames + 1
                     else:
                         cv2.putText(resized, 'Rock', (0, 100), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
                 else:
                     paper = 0
                     rock = 0
                     scissors = 0
+                    label_test = 3
+                    certain_frames = certain_frames + 1
 
-                # converting to grayscale:
+                if label_test == sheet.cell_value(num_frames, 1):
+                    cv2.putText(resized, 'Label Matched', (0, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 2)
+                    correct_frames = correct_frames + 1
 
             else:
                 cv2.drawContours(resized, [contours[ci]], 0, (0, 255, 0), 2)
                 cv2.drawContours(resized, [hull], 0, (0, 0, 255), 2)
 
-        G = cv2.cvtColor(resized2, cv2.COLOR_BGR2GRAY)
-        #G = resized2
-        # Smoothing the image:
-        #G = cv2.medianBlur(src=G, ksize=5)
-
-        # making a copy for drawing:
-        #Output = resized.copy()
-
-        dots = cv2.goodFeaturesToTrack(G, maxCorners=500, qualityLevel=0.000001, minDistance=10)
-
-        Z = np.float32(dots)
-        Z = np.transpose(Z)
-
-        #J, label, centre = cv2.kmeans(Z, K, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
-
-        # number of dots:
-        N = np.shape(dots)[0]
-
-        # drawing each dot:
-        for n in range(0, N):
-            j = int(dots[n, :, 0][0])
-            i = int(dots[n, :, 1][0])
-
-        # if this is not the first frame:
-        if f > 0:
-            # delete the dots:
-            del dots
-
-            # Calculate optical flow:
-            dots, status, error = cv2.calcOpticalFlowPyrLK(prevImg=prevFrame, nextImg=G, prevPts=prevPts,
-                                                           nextPts=None, **lk_params)
-
-            # number of dots:
-            N = np.shape(dots)[0]
-
-            # If there are dots to draw:
-            if N > 0:
-                # drawing each dot:
-                for n in range(0, N):
-                    j = int(dots[n, :, 0][0])
-                    i = int(dots[n, :, 1][0])
-
-                    if status[n][0] == 0:
-                        # if it didn't work, go back to the previous point:
-                        dots[n] = prevPts[n]
-
-                    if status[n][0] == 1:
-                        print()
-                        #Make the successful Lucas-Kanade dots red:
-                        #resized[i - 3:i + 3, j - 3:j + 3] = (0, 0, 255)
-
-        # Get the current frame and dots ready for the next round:
-        prevPts = dots.copy()
-        prevFrame = G.copy()
-
         cv2.imshow("FIRST", resized)
-        #cv2.imshow("second", thresh1)
-        # cv2.imshow("SECOND", firstFrame)
-        # cv2.imshow("ABS-DIFF", frameDelta)
-        # cv2.imshow("THRESH", thresh)
-        frame = cv2.flip(resized, 0)
+        # cv2.imshow("second", thresh1)
+        frame = resized
         out.write(frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -216,8 +180,13 @@ while check:
 
     else:
         print("done")
-        percent = (certain_frames/num_frames) * 100
-        print(percent)
+        print(num_frames)
+        print(certain_frames)
+        print(correct_frames)
+        percent_certain = (certain_frames/num_frames) * 100
+        print(percent_certain)
+        percent_correct = (correct_frames/num_frames) * 100
+        print(percent_correct)
         break
 
 vc.release()
