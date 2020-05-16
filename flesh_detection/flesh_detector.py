924ce7d7d4fcdbe512bf1a4945a58d812a4c9950
Peter Kinsella <pallesnik@gmail.com>
2020-05-16 19:09:29 +0100

    Cleaning up code

--- flesh_detection/flesh_detector.py
+++ flesh_detection/flesh_detector.py
@@ -1,26 +1,14 @@
-import argparse
-import datetime
-import time
 import cv2
 import numpy as np
 import math
-import matplotlib.pyplot as plt
 
 vc = cv2.VideoCapture("lubin.mp4")  # video capture [1]
-
-width = np.int(vc.get(cv2.CAP_PROP_FRAME_WIDTH))  # float
-height = np.int(vc.get(cv2.CAP_PROP_FRAME_HEIGHT))  # float
-Nframes = np.int(vc.get(cv2.CAP_PROP_FRAME_COUNT))
 FPS = np.int(vc.get(cv2.CAP_PROP_FPS))
 dim = (640, 480)  # resizing image
-last_position = [0] * 100
-
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 out = cv2.VideoWriter("output.mp4", fourcc, FPS, dim, True)
 (check, frame) = vc.read()
-points_to_track = [0] *30
-K = 20
-f = 0
+f = 0 
 lk_params = dict(winSize=(15,15),maxLevel=2,criteria=(cv2.TERM_CRITERIA_EPS|cv2.TERM_CRITERIA_COUNT,10,0.01))
 criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_MAX_ITER, 10, 0.1)
 num_frames = 0
@@ -28,9 +16,6 @@ certain_frames = 0
 paper = 0
 rock = 0
 scissors = 0
-def_rock = 0
-def_paper = 0
-def_scissors = 0
 
 while check:
     if frame is not None:
@@ -50,11 +35,9 @@ while check:
         thresh1 = cv2.bitwise_and(resized, resized, mask=thresh)
 
         contours, H = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
         contours = sorted(contours, key=cv2.contourArea, reverse=True)
-
         max_area = 0
-        k = 0
+
         for i in range(len(contours)):  # finding largest contour by area [3]
             contour = contours[i]
             area = cv2.contourArea(contour)
@@ -62,8 +45,6 @@ while check:
                 max_area = area
                 ci = i
         if len(contours) > 0:
-            (x, y, w, h) = cv2.boundingRect(contours[ci])
-            # cv2.rectangle(resized, (x, y), (x + w, y + h), (0, 255, 0), 2)
             moments = cv2.moments(contours[ci])
             if moments['m00'] != 0:  # this gives the centre of the moments [3]
                 cx = int(moments['m10'] / moments['m00'])  # cx = M10/M00
@@ -74,7 +55,6 @@ while check:
             hull2 = cv2.convexHull(contours[ci], returnPoints=False)
             defects = cv2.convexityDefects(contours[ci], hull2)
             num_def = 0
-            #print(defects)
 
             if defects is not None:
                 for i in range(defects.shape[0]):
@@ -94,8 +74,6 @@ while check:
                         if angle <= 90:
                             num_def += 1
                             cv2.circle(resized, far, 5, [255, 0, 0], -1)
-                            #points_to_track[k] = far
-                            #k = k + 1
 
                         cv2.line(num_def, start, end, [0, 255, 0], 2)
                         cv2.drawContours(resized, [contours[ci]], 0, (0, 255, 0), 2)
@@ -137,26 +115,16 @@ while check:
                     rock = 0
                     scissors = 0
 
-                # converting to grayscale:
-
             else:
                 cv2.drawContours(resized, [contours[ci]], 0, (0, 255, 0), 2)
                 cv2.drawContours(resized, [hull], 0, (0, 0, 255), 2)
 
         G = cv2.cvtColor(resized2, cv2.COLOR_BGR2GRAY)
-        #G = resized2
-        # Smoothing the image:
-        #G = cv2.medianBlur(src=G, ksize=5)
-
-        # making a copy for drawing:
-        #Output = resized.copy()
 
         dots = cv2.goodFeaturesToTrack(G, maxCorners=500, qualityLevel=0.000001, minDistance=10)
 
         Z = np.float32(dots)
         Z = np.transpose(Z)
-
-        #J, label, centre = cv2.kmeans(Z, K, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
 
         # number of dots:
         N = np.shape(dots)[0]
@@ -174,7 +142,6 @@ while check:
             # Calculate optical flow:
             dots, status, error = cv2.calcOpticalFlowPyrLK(prevImg=prevFrame, nextImg=G, prevPts=prevPts,
                                                            nextPts=None, **lk_params)
-
             # number of dots:
             N = np.shape(dots)[0]
 
@@ -190,19 +157,14 @@ while check:
                         dots[n] = prevPts[n]
 
                     if status[n][0] == 1:
-                        print()
                         #Make the successful Lucas-Kanade dots red:
-                        #resized[i - 3:i + 3, j - 3:j + 3] = (0, 0, 255)
+                        resized[i - 3:i + 3, j - 3:j + 3] = (0, 0, 255)
 
         # Get the current frame and dots ready for the next round:
         prevPts = dots.copy()
         prevFrame = G.copy()
 
         cv2.imshow("FIRST", resized)
-        #cv2.imshow("second", thresh1)
-        # cv2.imshow("SECOND", firstFrame)
-        # cv2.imshow("ABS-DIFF", frameDelta)
-        # cv2.imshow("THRESH", thresh)
         frame = cv2.flip(resized, 0)
         out.write(frame)
 
@@ -210,9 +172,6 @@ while check:
             break
 
         rval, frame = vc.read()
-
-    #except:
-    #    continue
 
     else:
         print("done")
